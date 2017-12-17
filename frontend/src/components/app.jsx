5a7448936b76a33b143b5c5932eee8ea266c4aac
wesrobinson12 <wpr124@gmail.com>
2017-12-17 18:26:38 -0500

    Add frontend setup with hello world

---
+++ frontend/src/components/app.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import useSheet from 'react-jss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+}
+
+const styles = {
+
+};
+
+export default App;
